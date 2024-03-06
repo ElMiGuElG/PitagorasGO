cc0ae0fad37304bdc4b2960eb93b35f656c1cb5e
Miguel Ángel Galeano <113398998+ElMiGuElG@users.noreply.github.com>
2024-03-06 06:31:45 -0500

    Add main menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.airbnb.android:lottie:6.4.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
 }
