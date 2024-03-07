8af5558f02c51bd52c9ac58c60507c96cc59f528
Miguel Ángel Galeano <113398998+ElMiGuElG@users.noreply.github.com>
2024-03-06 20:29:30 -0500

    Finished part of the perimeter

--- app/src/main/java/co/galeano/pitagorasgo/MainMenuActivity.java
+++ app/src/main/java/co/galeano/pitagorasgo/MainMenuActivity.java
@@ -1,15 +1,58 @@
 package co.galeano.pitagorasgo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.animation.content.Content;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    //----- Declaration of variables  -----
+    private CardView viewFunction;
+    private CardView viewArea;
+    private CardView viewPerimeter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        initComponent();
+        initListener();
+    }
+
+    private void initComponent() {
+        viewFunction = findViewById(R.id.viewFunction);
+        viewArea = findViewById(R.id.viewArea);
+        viewPerimeter = findViewById(R.id.viewPerimeter);
+    }
+
+    private void initListener() {
+        viewFunction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FunctionsActivity.class);
+                startActivity(intent);
+            }
+        });
+        viewArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AreaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        viewPerimeter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PerimeterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/co/galeano/pitagorasgo/PerimeterActivity.java
+++ app/src/main/java/co/galeano/pitagorasgo/PerimeterActivity.java
@@ -1,14 +1,60 @@
 package co.galeano.pitagorasgo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class PerimeterActivity extends AppCompatActivity {
+    private EditText editNumber;
+    private AppCompatButton btn_Calcular;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perimeter);
+
+        initComponent();
+        initListener();
+    }
+
+    private void initComponent() {
+        editNumber = findViewById(R.id.editNumber);
+        btn_Calcular = findViewById(R.id.btn_Calcular);
+        tvResult = findViewById(R.id.tvResult);
+    }
+
+    private void initListener() {
+        btn_Calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputText = editNumber.getText().toString();
+                if (!inputText.isEmpty()) {
+                    double radius = Double.parseDouble(inputText);
+                    calculatePerimeter(radius);
+                }else {
+                    tvResult.setText("Error");
+                }
+            }
+        });
+    }
+
+    private void calculatePerimeter (double radius) {
+        if (radius > 0) {
+            double perimeter = 2 * Math.PI * radius;
+            DecimalFormat decimalFormat = new DecimalFormat("#.##");
+            String formattedResult = decimalFormat.format(perimeter);
+            tvResult.setText(String.valueOf(formattedResult));
+        } else {
+            tvResult.setText("Error");
+        }
     }
 }
